73a526f91f909920c7a7cb835cd369502eb46b36
kbledzki <krzysztofbledzki@gmail.com>
2023-06-20 19:52:14 +0200

    project spring initiated

---
+++ src/test/java/com/java/ProductRest/ProductRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.ProductRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
